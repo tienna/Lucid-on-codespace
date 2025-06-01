aab217562e70e4b85cd41edcfe7cd86ff1679d6b
tienna <tienna@gmail.com>
2025-06-01 21:48:01 +0700

    Refactor minting and burning logic in NFT scripts; update parameter handling and improve code structure

--- aiken/04_mint_NFT/burn_unlock.ts
+++ aiken/04_mint_NFT/burn_unlock.ts
@@ -16,57 +16,55 @@ const lucid = new Lucid({
   });
 lucid.selectWalletFromSeed(Bob_mnonic);
   
-  const wallet_address = await lucid.wallet.address();
-  console.log(`Địa chỉ ví là: ${wallet_address}`);
-  const payment_hash =  Addresses.inspect(wallet_address).payment?.hash;
+const wallet_address = await lucid.wallet.address();
+console.log(`Địa chỉ ví là: ${wallet_address}`);
+const payment_hash =  Addresses.inspect(wallet_address).payment?.hash;
+
+
+const token_name = fromText("BK03_0003");
+const fee_value = 10000000n;
+const payment_credential =  Addresses.inspect("addr_test1qqew6jaz63u389gwnp8w92qntetzxs6j9222pn4cnej672vazs7a6wnrseqggj4d4ur43yq9e23r4q0m879t7efyhzjq8mvzua").payment?.hash;
+console.log(payment_credential);
 
 const validator = await readValidator();
-const parameterized_cbor = applyParamsToScript([fromText("BK03_0001")],validator.script);
+// const parameterized_cbor = applyParamsToScript([token_name,fee_value,payment_credential],validator.script);
+// const parameterized_script = lucid.newScript({
+//   type: "PlutusV3",
+//   script: parameterized_cbor,
+// });
+const Params  = [Data.Bytes(), Data.Integer(), Data.Bytes()];
 const parameterized_script = lucid.newScript({
   type: "PlutusV3",
-  script: parameterized_cbor,
-});
+  script: validator.script,
+},[token_name,fee_value,payment_credential]
+,Params
+);
+
 const scriptAddress =parameterized_script.toAddress();
 console.log(`Địa chỉ Parameterized script là: ${scriptAddress}`);
 const policyId = parameterized_script.toHash();
-const unit = policyId + fromText("BK03_0001");
+const unit = policyId + fromText("BK03_0003");
 const utxos = await lucid.utxosAt(wallet_address);
-const utxo1 = utxos.find(u => u.txHash === "fc83d672482b12298feae43a5ef90a63551f5df96acdbf54a21c0c1f883d8eba" && u.outputIndex === 1);
-console.log(utxo1);
+// const utxo1 = utxos.find(u => u.txHash === "fc83d672482b12298feae43a5ef90a63551f5df96acdbf54a21c0c1f883d8eba" && u.outputIndex === 1);
+
 const utxo = utxos.find(u => u.assets[unit] && u.assets[unit] >= 1n);
 if (!utxo) throw new Error("Không tìm thấy UTXO chứa NFT");
 
-
+console.log(utxo);
 // 1 tương ứng với vị trí thứ 2 của của redeemer trong aiken ==Burn
 const mintRedeemer = Data.to(new Constr(1, []));
 const tx = await lucid
     .newTx()
     .mint({[unit]: -1n},mintRedeemer)
     .collectFrom([utxo])
-    .collectFrom([utxo1])
+    .payTo("addr_test1qqew6jaz63u389gwnp8w92qntetzxs6j9222pn4cnej672vazs7a6wnrseqggj4d4ur43yq9e23r4q0m879t7efyhzjq8mvzua", { lovelace: 10000000n })
+    // .collectFrom([utxo1])
     .attachScript(parameterized_script)
-    .addSigner(payment_hash)
+    // .addSigner(payment_hash)
     .commit();
 const signedTx = await tx.sign().commit();
 const txHash = await signedTx.submit();
 console.log(`A NFT was Burnt at tx:    https://preview.cexplorer.io/tx/${txHash} `);
-
-// ========================= code thay doi tu day==============================
-// const redeemer = Data.to(new Constr(0, [utf8ToHex("Hello, BK03")]));
-// console.log(`Redeemer sẽ được truyền vào SC là: ${redeemer}`);
-// const validator = await readValidator();
-// console.log(validator);
-// const parameterizedScript = applyParamsToScript([payment_hash],validator.script,);
-// const script = lucid.newScript({
-//     type: "PlutusV3",
-//     script: parameterizedScript,
-//   });
-// const scriptAddress=script.toAddress();
-// console.log(`Địa chỉ script là: ${scriptAddress}`);
-
-
-  
-
 
 
  // Đọc validator từ plutus.json
--- aiken/04_mint_NFT/mint_lock.ts
+++ aiken/04_mint_NFT/mint_lock.ts
@@ -1,20 +1,20 @@
-import {  Blockfrost, Lucid, Addresses,fromHex,toHex,Data,paymentCredentialOf, Constr,fromText,applyParamsToScript } from "https://deno.land/x/lucid@0.20.9/mod.ts";
+import { Blockfrost, Lucid, Addresses, fromHex, toHex, Data, paymentCredentialOf, Constr, fromText, applyParamsToScript } from "https://deno.land/x/lucid@0.20.9/mod.ts";
 // import { applyParamsToScript,} from "@lucid-evolution/lucid";
 import "jsr:@std/dotenv/load";
-import * as fs from 'node:fs';
 import * as cbor from "https://deno.land/x/cbor@v1.4.1/index.js";
 // import { getScript, getTxBuilder, getWalletInfoForTx, wallet } from "./common";
+
 // Lấy các biến từ env
 const Bob_mnonic = Deno.env.get("MNEMONIC");
 const BLOCKFROST_ID = Deno.env.get("BLOCKFROST_ID");
-const BLOCKFROST_NETWORK = Deno.env.get("BLOCKFROST_NETWORK")
+const BLOCKFROST_NETWORK = Deno.env.get("BLOCKFROST_NETWORK");
 
 const lucid = new Lucid({
-    provider: new Blockfrost(
-      BLOCKFROST_NETWORK,
-      BLOCKFROST_ID,
-    ),
-  });
+  provider: new Blockfrost(
+    BLOCKFROST_NETWORK,
+    BLOCKFROST_ID,
+  ),
+});
 lucid.selectWalletFromSeed(Bob_mnonic);
 
 //====================xử lý param==================
@@ -23,49 +23,56 @@ const fee_value = 10000000n;
 // const payment_credential = paymentCredentialOf("addr_test1qqew6jaz63u389gwnp8w92qntetzxs6j9222pn4cnej672vazs7a6wnrseqggj4d4ur43yq9e23r4q0m879t7efyhzjq8mvzua").hash;
 // 32ed4ba2d47913950e984ee2a8135e562343522a94a0ceb89e65af29
 
-const payment_credential =  Addresses.inspect("addr_test1qqew6jaz63u389gwnp8w92qntetzxs6j9222pn4cnej672vazs7a6wnrseqggj4d4ur43yq9e23r4q0m879t7efyhzjq8mvzua").payment?.hash;
-
+const payment_credential = Addresses.inspect(
+  "addr_test1qqew6jaz63u389gwnp8w92qntetzxs6j9222pn4cnej672vazs7a6wnrseqggj4d4ur43yq9e23r4q0m879t7efyhzjq8mvzua"
+).payment?.hash;
 console.log(payment_credential);
 
 const validator = await readValidator();
-const parameterized_cbor = applyParamsToScript([token_name,fee_value,payment_credential],validator.script);
-const parameterized_script = lucid.newScript({
-  type: "PlutusV3",
-  script: parameterized_cbor,
-});
-const scriptAddress =parameterized_script.toAddress();
+// const parameterized_cbor = applyParamsToScript([token_name,fee_value,payment_credential],validator.script);
+// const parameterized_script = lucid.newScript({
+//   type: "PlutusV3",
+//   script: parameterized_cbor,
+// });
+const Params = [Data.Bytes(), Data.Integer(), Data.Bytes()];
+const parameterized_script = lucid.newScript(
+  {
+    type: "PlutusV3",
+    script: validator.script,
+  },
+  [token_name, fee_value, payment_credential],
+  Params
+);
+
+const scriptAddress = parameterized_script.toAddress();
 console.log(`Địa chỉ Parameterized script là: ${scriptAddress}`);
 const policyId = parameterized_script.toHash();
-
 const unit = policyId + fromText("BK03_0003");
-
-
 
 const mintRedeemer = Data.to(new Constr(0, []));
 // const mintRedeemer = Data.void()
 const tx = await lucid
-      .newTx()
-      .mint({[unit]: 1n}, mintRedeemer)
-      .payTo("addr_test1qqew6jaz63u389gwnp8w92qntetzxs6j9222pn4cnej672vazs7a6wnrseqggj4d4ur43yq9e23r4q0m879t7efyhzjq8mvzua", { lovelace: 10000000n })
-      .attachScript(parameterized_script)
-      .commit();
+  .newTx()
+  .mint({ [unit]: 1n }, mintRedeemer)
+  .payTo(
+    "addr_test1qqew6jaz63u389gwnp8w92qntetzxs6j9222pn4cnej672vazs7a6wnrseqggj4d4ur43yq9e23r4q0m879t7efyhzjq8mvzua",
+    { lovelace: 10000000n }
+  )
+  .attachScript(parameterized_script)
+  .commit();
+
 const signedTx = await tx.sign().commit();
-
-
 await Deno.writeTextFile("BK03_0003-signedTx.cbor", signedTx);
 const txHash = await signedTx.submit();
 console.log(`A NFT was mint at tx:    https://preview.cexplorer.io/tx/${txHash} `);
 
-
-
- 
 //===============Đọc mã CBOR của SC  ============================
 async function readValidator(): Promise<SpendingValidator> {
   const validator = JSON.parse(await Deno.readTextFile("plutus.json")).validators[0];
-      return {
-        type: "PlutusV3",
-        script: toHex(cbor.encode(fromHex(validator.compiledCode))),
-      };
-    }
+  return {
+    type: "PlutusV3",
+    script: toHex(cbor.encode(fromHex(validator.compiledCode))),
+  };
+}
 
 
